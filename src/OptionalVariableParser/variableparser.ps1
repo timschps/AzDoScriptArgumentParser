e70d1e6f2ecf730f0cda75297e7ec4fab9939445
Tim Schaeps <timschaeps@gmail.com>
2021-04-08 18:31:37 +0200

    cleanup

--- src/OptionalVariableParser/variableparser.ps1
+++ src/OptionalVariableParser/variableparser.ps1
@@ -25,31 +25,32 @@ try {
     $ParsedVariableName = Get-VstsInput -Name ParsedVariableName -Require
 
     Write-Host "-----------------------------------------------------"
-    Write-Host "This task allows you to specify an optional variable that you can specify on build level. when specified," 
-    Write-Host "it will override the default value that can also be specified here." 
+    Write-Host "This task allows you to specify an optional variable if you need one in your pipeline."
+    Write-Host "You can specify a variable name with a default value, which in turn can be overridden on" 
+    Write-Host "build/pipeline level. (when specified)" 
+    Write-Host "It will override the default value that can also be specified here." 
     
-    Write-Host "This task will try to retreive a value for variable: '$VariableName'"
-    Write-Host "So, if you want to modify the optional variable, 
+    Write-Host "This task will try to retreive a value for variable: '$VariableName' which can be used to"
+    Write-Host "override the default value ($DefaultValue) So, if you want to override this optional variable, 
     then all you need to do is to specify a build variable (with name $VariableName) and give it a value!"
     Write-Host "-----------------------------------------------------"
     
-
-    
-    
+    Write-Host "-----------------------------------------------------"
+    Write-Host "Starting!"
+    Write-Host "-----------------------------------------------------"
     Write-Host "Progress: Checking if variable '$VariableName' has been specified in the build variables"
     $VariableValue = [Environment]::GetEnvironmentVariable($VariableName)
     if($VarableValue){ 
         $VariableValue = $VarableValue
+        Write-Host "I found build variable with name: $VariableName. The value is $VariableValue"
         }else{
         $VarableValue = $DefaultValue
+        Write-Host "I found no build variable with name: $VariableName."
         }
+        Write-Host "-----------------------------------------------------"
 
     Set-VstsTaskVariable -Name $ParsedVariableName -Value $VariableValue
     # Output the message to the log.
-    Write-Host "-----------------------------------------------------"
-    Write-Host "Here is a resume of what I found/did:"
-    Write-Host "I had to look for a build variable with name: $VariableName"
-    Write-Host "-----------------------------------------------------"
 } finally {
     Trace-VstsLeavingInvocation $MyInvocation
 }
